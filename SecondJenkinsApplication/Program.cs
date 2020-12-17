6147267e102c14b5314a63f6236f5d341155dd3a
NeelBafna4 <76032699+NeelBafna4@users.noreply.github.com>
2020-12-17 14:45:14 +0530

    Update Program.cs

--- SecondJenkinsApplication/Program.cs
+++ SecondJenkinsApplication/Program.cs
@@ -11,7 +11,7 @@ namespace SecondJenkinsApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Second Jenikins Project");
-             Console.WriteLine("Error")
+             Console.WriteLine("Error");
         }
     }
 }
